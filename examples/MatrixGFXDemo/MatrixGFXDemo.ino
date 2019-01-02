a49c4fa563c2d55d7799c732cb9053636d86aabd
Marc MERLIN <marc_soft@merlins.org>
2019-01-01 23:12:38 -0800

    Added speed test for teensy 3.6.

--- examples/MatrixGFXDemo/MatrixGFXDemo.ino
+++ examples/MatrixGFXDemo/MatrixGFXDemo.ino
@@ -4,6 +4,7 @@
 
 #include <Adafruit_GFX.h>
 #include <SmartMatrix_GFX.h>
+#include <SmartLEDShieldV4.h>  // comment out this line for if you're not using SmartLED Shield V4 hardware (this line needs to be before #include <SmartMatrix3.h>)
 #include <SmartMatrix3.h>
 
 // Choose your prefered pixmap
@@ -24,8 +25,8 @@
 #define kMatrixHeight 64       // known working: 16, 32, 48, 64
 const uint8_t kRefreshDepth = 24;       // known working: 24, 36, 48
 const uint8_t kDmaBufferRows = 2;       // known working: 2-4, use 2 to save memory, more to keep from dropping frames and automatically lowering refresh rate
-const uint8_t kPanelType = SMARTMATRIX_HUB75_32ROW_MOD16SCAN;   // use SMARTMATRIX_HUB75_16ROW_MOD8SCAN for common 16x32 panels
-//const uint8_t kPanelType = SMARTMATRIX_HUB75_64ROW_MOD32SCAN;
+//const uint8_t kPanelType = SMARTMATRIX_HUB75_32ROW_MOD16SCAN;   // use SMARTMATRIX_HUB75_16ROW_MOD8SCAN for common 16x32 panels
+const uint8_t kPanelType = SMARTMATRIX_HUB75_64ROW_MOD32SCAN;
 const uint8_t kMatrixOptions = (SMARTMATRIX_OPTIONS_NONE);      // see http://docs.pixelmatix.com/SmartMatrix for options
 const uint8_t kBackgroundLayerOptions = (SM_BACKGROUND_OPTIONS_NONE);
 
@@ -685,6 +686,8 @@ void setup() {
 // You can't use millis to time frame fresh rate because it uses cli() which breaks millis()
 // So I use my stopwatch to count 1000 displays and that's good enough
 // 500 loops of 2 updates each is 13.3s for 1000 updates or 75 updates per second for 4096 LEDs
+// for ESP32
+// For some reason it's much faster on teensy 3.6, 18 seconds or 228fps
     for (uint16_t i=0; i<500; i++) { 
 	matrix->fillScreen(LED_BLUE_HIGH);
 	matrix->show();
@@ -695,7 +698,7 @@ void setup() {
 
     // This counts but also shows bleeding between pixels and lines.
     Serial.println("Count pixels. This is slow, you may want to comment me out");
-    //count_pixels();
+    count_pixels();
     Serial.println("Count pixels done");
     delay(1000);
 }
