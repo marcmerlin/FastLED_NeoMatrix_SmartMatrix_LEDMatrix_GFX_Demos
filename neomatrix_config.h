be9a81b56d894f30f6950f2be125004102444cb4
Marc MERLIN <marc_soft@merlins.org>
2020-09-01 15:26:47 -0700

    changed size of ArduinoOnPC.

--- neomatrix_config.h
+++ neomatrix_config.h
@@ -723,8 +723,8 @@ uint32_t tft_spi_speed;
 	const uint16_t MATRIX_TILE_WIDTH =  64; // width of EACH NEOPIXEL MATRIX (not total display)
 	const uint16_t MATRIX_TILE_HEIGHT=   1; // height of each matrix
     #else
-	const uint16_t MATRIX_TILE_WIDTH = 128; // width of EACH NEOPIXEL MATRIX (not total display)
-	const uint16_t MATRIX_TILE_HEIGHT= 192; // height of each matrix
+	const uint16_t MATRIX_TILE_WIDTH = 384; // width of EACH NEOPIXEL MATRIX (not total display)
+	const uint16_t MATRIX_TILE_HEIGHT= 256; // height of each matrix
     #endif
     const uint8_t MATRIX_TILE_H     = 1;  // number of matrices arranged horizontally
     const uint8_t MATRIX_TILE_V     = 1;  // number of matrices arranged vertically
