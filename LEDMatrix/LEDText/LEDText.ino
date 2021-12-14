0694b6a76ab627932dec403051c6314d67cadb44
Marc MERLIN <marc_soft@merlins.org>
2021-12-13 20:12:31 -0800

    LEDText example.

---
+++ LEDMatrix/LEDText/LEDText.ino
@@ -0,0 +1,55 @@
+#define LEDMATRIX
+// Replace this with another backend if you wish
+#define ILI9341
+#include "neomatrix_config.h"
+#include <LEDText.h>
+//#include <Font12x16.h>
+#include <ComicSansP24.h>
+//#include <FontMatrise.h>
+
+
+cLEDText ScrollingMsg;
+
+const unsigned char TxtDemo[] = { EFFECT_FRAME_RATE "\x00"
+                                  EFFECT_HSV_AH "\x00\xff\xff\xff\xff\xff"
+                                  EFFECT_SCROLL_LEFT "   The "
+                                  EFFECT_SCROLL_UP "Quick "
+                                  EFFECT_SCROLL_LEFT "Brown "
+                                  EFFECT_SCROLL_DOWN "Fox"
+                                  EFFECT_SCROLL_LEFT " Jumps "
+                                  EFFECT_SCROLL_UP "Over  "
+                                  EFFECT_SCROLL_LEFT "The "
+                                  EFFECT_SCROLL_DOWN "Lazy  "
+                                  EFFECT_SCROLL_LEFT "Dog " };
+uint16_t Options;
+
+void setup()
+{
+    matrix_setup();
+
+    matrix->clear();
+
+    //ScrollingMsg.SetFont(MatriseFontData);
+    ScrollingMsg.SetFont(ComicSansP24Data);
+    ScrollingMsg.Init(&ledmatrix, mw, ScrollingMsg.FontHeight() + 1, 0, 0);
+    ScrollingMsg.SetText((unsigned char *)TxtDemo, sizeof(TxtDemo) - 1);
+    ScrollingMsg.SetTextColrOptions(COLR_RGB | COLR_SINGLE, 0xff, 0x00, 0xff);
+    Options = INSTANT_OPTIONS_MODE;
+    ScrollingMsg.SetOptionsChangeMode(Options);
+}
+
+
+void loop()
+{
+    if (ScrollingMsg.UpdateText() == -1)
+    {
+	ScrollingMsg.SetText((unsigned char *)TxtDemo, sizeof(TxtDemo) - 1);
+	Options ^= INSTANT_OPTIONS_MODE;
+	ScrollingMsg.SetOptionsChangeMode(Options);
+    }
+    else
+    {
+	matrix->show();
+	delay(30);
+    }
+}
